a67cf551351ee51572d35716e041e1a6020f1810
Ryan Yep <ryan.yep@gmail.com>
2015-08-26 11:35:05 -0500

    Day 3 Class Fun

---
+++ class-fun.rb
@@ -0,0 +1,78 @@
+# class Student
+#   CAMPUS = 'ATX'
+#   def sleep
+#     puts "Goes to bed"
+#   end
+
+#   def study
+#     puts "studies"
+#   end
+
+# end
+
+# VERSION = "1.1.1"
+
+
+# # Creating an instance of the class Student and
+# # Assigning it to the variable 'jacob'
+# # Without this you wouldn't be able to call the method
+# jacob = Student.new
+# jacob.study
+# jacob.sleep
+
+# VERSION
+# CAMPUS
+# Student::CAMPUS #to access CAMPUS you have to go through Student
+
+# # ------------================---------------
+
+# class Student
+
+#   def initialize
+#     @bed_time = "Didn't sleep yet"
+#     @awake_time = "Not yet"
+
+#   def wakes_up
+#     @awake_time = Time.now
+#     puts "Went to bed at #{@bed_time} and woke up at #{@awake_time}"
+#     "Wakes up"
+#   end
+
+#   def sleep
+#     @bed_time = Time.now
+#     puts "Goes to bed at #{@bed_time}"
+#   end
+
+#   def study
+#     puts "studies"
+#   end
+# end
+
+# VERSION = "1.1.1"
+
+
+# # Creating an instance of the class Student and
+# # Assigning it to the variable 'jacob'
+# # Without this you wouldn't be able to call the method
+# jacob = Student.new
+# sarah = Student.new
+# jacob.sleep
+# sarah.sleep
+
+# --------------===================----------------
+
+class Person
+
+  def initialize
+  end
+
+end
+
+# Classes are Factories (Nouns)
+# Their methods are Actions (Verbs)
+# Their instance variables are properties (Data or State)
+
+
+
+
+
